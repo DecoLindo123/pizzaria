eb5fe72eced990451a3a5a02a2925e632a8369a5
DecoLindo123 <103005716+DecoLindo123@users.noreply.github.com>
2022-09-21 19:15:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+menuListArray = ["Pizza Vegetariana",
+"Pizza de Calabresa",
+"Pizza Portuguesa",
+"Pizza Marguenita",
+"Pizza de Lombinho"
+                    ];
+
+function getMenu(){
+var htmldata;
+menuListArray.short();
+for(var i=0;i<menuListArray.legth;i++) {
+    htmldata=htmldata+menuListArray[i] + '<br>'
+ }
+ document.getElementById("displayMenu").innerHTML = htmldata;
+}
+
+
+function addItem(){
+var htmldata;
+var imgtags='<img id="im1" src="images/pizzaImg.png"/>' 
+var item=document.getElementById("addItem").value; 
+menuListArray.sort(); htmldata="" 
+for(var i=0;i<menuListArray.length;i++){ 
+    htmldata=htmldata+imgtags+ menuListArray[i]+'<br>'; }
+     document.getElementById("displayAddedMenu").innerHTML = htmldata;
+     
+
+
+}
+
+function addTop(){
+    var item=document.getElementById("addItem").value; 
+    menuListArray.push(item); 
+    addItem();
+}
